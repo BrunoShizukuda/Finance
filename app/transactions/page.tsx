5db1bbf3b017d516ace90d2b639bd44feca148cf
Bruno Shizukuda <shizukudabruno@gmail.com>
2024-11-07 10:03:57 -0300

    adicionando edicão e adicão nos registros

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,21 +1,20 @@
-
-import { db } from '../_lib/prisma';
-import { DataTable } from '../_components/ui/data-table';
-import { TransactionColumns } from './_columns';
-import AddTransactionButton from '../_components/ui/add-transaction-button';
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { TransactionColumns } from "./_columns";
+import AddTransactionButton from "../_components/ui/add-transaction-button";
 
 const TransactionPage = async () => {
-    const transactions = await db.transaction.findMany({});
-    return (
-        <div className='p-6 space-y-6'>
-            {/* Titulo e Botão */}
-            <div className='flex w-full justify-between items-center '>
-                <h1 className='text-2xl font-bold'>Transações</h1>
-                <AddTransactionButton/>
-            </div>
-            <DataTable columns={TransactionColumns} data={transactions}/>
-        </div>
-    );
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      {/* Titulo e Botão */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <AddTransactionButton />
+      </div>
+      <DataTable columns={TransactionColumns} data={transactions} />
+    </div>
+  );
 };
 
 export default TransactionPage;
